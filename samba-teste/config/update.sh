f9c84d5a8c9ba35161a791bc7887e15c178d9e1a
Carlos Eduardo Veloso <caduveloso@gmail.com>
2020-02-27 20:54:09 -0300

    Add files via upload

---
+++ samba-teste/config/update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -eux
+#update
+#sudo yum update -y
+
+# Install Python.
+yum -y install python3 python3-pip
+alternatives --set python /usr/bin/python3
+
+# Install Ansible.
+pip3 install ansible
